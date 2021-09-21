62b4899485a095de06aa35d2258bd46589698d19
Tony Masci <tony.masci@rapsodoo.com>
2021-09-21 11:48:03 +0200

    asddsa

--- release_odoo_accounting/module.py
+++ release_odoo_accounting/module.py
@@ -11,26 +11,15 @@ _logger = logging.getLogger(__name__)
 stop_run = False
 
 if len(sys.argv) > 1 and sys.argv[1]:
-    if len(sys.argv) != 3:
-        print('Inserire questi parametri: True percorso_delle_cartelle_dei_progetti')
-        stop_run = True
     path_file_xml = 'settings.xml'
-    if not stop_run:
-        folder_projects = sys.argv[2]
-    else:
-        folder_projects = ''
 else:
     path_file_xml = os.path.join(tempfile.gettempdir(), 'settings.xml')
-    folder_projects = ''
 
 if not stop_run:
     print('path_file_xml: {}'.format(path_file_xml))
     
     with open(path_file_xml, 'r') as myfile:
         data = xmltodict.parse(myfile.read())
-    print(os.getcwd())
-    os.chdir('cd {}'.format(folder_projects))
-    print(os.getcwd())
     
     for repo in data['repositories']:
         os.chdir('cd {}'.format(data['repositories'][repo]['name']))
