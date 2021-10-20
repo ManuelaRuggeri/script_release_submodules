23fd0a022041fa287d026b7dae756cfa1a4eaa4a
Tony Masci <tony.masci@rapsodoo.com>
2021-10-20 16:55:02 +0200

    [release_rapsodoo_accounting][FIX]Fixed  - Changed "-ld" to "-l"  - Changed "setting_log" in "setup_log"  - Name of file log

--- release_rapsodoo_accounting/module.py
+++ release_rapsodoo_accounting/module.py
@@ -16,7 +16,7 @@ from github import Github
 
 description = 'Update submodules for rapsodoo-accounting'
 version = '%(prog)s 1.0'
-usage = '%(prog)s -t githubToken [-f path_file_xml] [-ld log_dir_name]'
+usage = '%(prog)s -t githubToken [-f path_file_xml] [-l log_dir_name]'
 
 def options():
     parser = argparse.ArgumentParser(
@@ -32,7 +32,7 @@ def options():
         action='store', nargs='?', help="Path of settings.xml file, if you don't pass it default is 'settings.xml'" 
     )
     parser.add_argument(
-        '-ld', '--log-dir-path', dest='log_dir_name', required=False,
+        '-l', '--log-dir-path', dest='log_dir_name', required=False,
         action='store', nargs='?', help="Path of dir log , if you don't pass it default is 'dir_log'" 
     )
     args = parser.parse_args()
@@ -42,10 +42,10 @@ def create_log_and_print(logger, msg):
     print(msg)
     logger.info(msg)
 
-def setting_log(log_dir_name):
+def setup_log(log_dir_name):
     if not os.path.isdir(log_dir_name):
         os.mkdir(log_dir_name)
-    logfile = "{}_{:%Y%m%d}.log".format('Pippo',date.today())
+    logfile = "{}_{:%Y%m%d}.log".format('release_rapsodoo_accounting',date.today())
     logfile = os.path.join(log_dir_name, logfile)
     logger = logging.getLogger('global log')
     logger.setLevel(logging.DEBUG)
@@ -67,7 +67,7 @@ def main():
     path_file_xml = 'settings.xml' if not bool(args.path_file_xml) else args.path_file_xml
     token = args.token
     
-    logger = setting_log(log_dir_name)
+    logger = setup_log(log_dir_name)
     
     with open(path_file_xml, 'r') as myfile:
         data = xmltodict.parse(myfile.read())
